39b2562c59504924d02024a640a29dbfd2607dc6
Your Name <you@example.com>
2019-03-27 11:26:47 +0800

    调用浏览器打开默认页面

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/browser"
 	"io"
 	"myProject/corll"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
@@ -49,4 +52,19 @@ func main() {
 
 	router.Run(":8888") // listen and serve on 0.0.0.0:8080
 	fmt.Println("strart ......")
+
+	//调用浏览器打开页面
+	//OpenUrl("https://www.baidu.com")
+	//browser.OpenURL("http://www.baidu.com")
+}
+
+var commands = map[string]string{
+	"windows": "cmd /c start",
+	"darwin":  "open",
+	"linux":   "xdg-open",
+}
+
+func OpenUrl(uri string) {
+	run, _ := commands[runtime.GOOS]
+	exec.Command(run, uri).Start()
 }
