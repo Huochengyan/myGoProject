bc40482b989da4c537917b9508bed924314412e5
huochengyan <929914120@qq.com>
2020-01-19 16:56:58 +0800

    mongo  add password auth

---
+++ views/test/index_files/site.min.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$("img.lazy").unveil(),$(".projects .thumbnail").matchHeight({byRow:!0,property:"height",target:null,remove:!1}),$.scrollUp({scrollName:"scrollUp",topDistance:"300",topSpeed:300,animation:"fade",animationInSpeed:200,animationOutSpeed:200,scrollText:'<i class="fa fa-angle-up"></i>',activeOverlay:!1})});
