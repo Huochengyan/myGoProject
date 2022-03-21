c24e0dbb17b0640cb92448c265abc7fe0f809768
huochengyan <929914120@qq.com>
2022-03-21 18:03:06 +0800

    create project

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/go-ini/ini"
 	"myGoProjectNew/myProjectUtils"
 	"myGoProjectNew/routers"
-	"time"
 )
 
 func main() {
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + "strart ......")
-	flag.Parse()
+	//now:=time.Now()
+	//fmt.Println(now.Format("2006-01-02 15:04:05") + "strart ......")
+	//flag.Parse()
 
 	cfg, err := ini.Load("conf/app.ini")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 
 	myProjectUtils.Config = cfg
@@ -27,7 +26,7 @@ func main() {
 	port := cfg.Section("http").Key("port").String()
 	err1 := router.Run(port)
 	if err1 != nil {
-		panic(err1)
+		fmt.Println(err)
 	}
 	fmt.Println("strart ......")
 	fmt.Println("open", "http://127.0.0.1:8888/myproject/")
